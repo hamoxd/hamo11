1f5c9527e9fc9273ec55393a8ee31a9826b12048
hamoxd <39615915+hamoxd@users.noreply.github.com>
2018-06-18 04:03:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,38 @@ client.on('message', message => {
         message.reply('pong');
       }
 });
+
+
+client.on('message', message => {
+     if (message.content === "رابط") {
+     let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#9B59B6")
+  .addField(" Done | تــــم" , " |  تــــم ارســالك في الخــاص")
+
+client.on("message", message => {
+ if (message.content === "رابط") {
+        message.react("😘")
+           message.react("😵")
+  const embed = new Discord.RichEmbed() 
+      .setColor("#ffff00")
+      .setThumbnail(message.author.avatarURL)
+      .setDescription(`
+***WELCOME TO ZOMBIE***
+https://discord.gg/2UJV6Hp
+message.author.sendEmbed(embed)
+}
+}); 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Z_4Ever`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
